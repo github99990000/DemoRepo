0bca73b1a1e10704dc0767992991654054f62533
govind <fatinggovind512@gmail.com>
2019-09-25 10:48:07 +0530

    first git comit for my selenium code

---
+++ src/main/java/com/qa/Libs/TestBase.java
@@ -0,0 +1,10 @@
+package com.qa.Libs;
+
+public class TestBase {
+	
+	public void test()
+	{
+		System.out.println("test method");
+	}
+
+}
---
+++ src/main/java/com/qa/utility/Testutil.java
@@ -0,0 +1,10 @@
+package com.qa.utility;
+
+public class Testutil {
+	
+	public void testutil()
+	{
+		System.out.println("test util");
+	}
+
+}
